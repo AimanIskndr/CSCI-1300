cc22fc6b51dab1b7e77c877264b9e9fe1b902457
Aiman Iskandar <esxkandar14@gmail.com>
2024-02-23 22:33:21 +0800

    I made this template since the question is tedious to solve

---
+++ ProjectEuler17.java
@@ -0,0 +1,53 @@
+/* AimanIskndr */
+import java.util.*;
+
+public class ProjectEuler17 {
+
+    static String[] nums = new String[101];
+
+    public static void solve() {
+        
+        long sum = 0;
+
+        /*
+        SOLVE HERE
+        */
+
+        System.out.println(sum);
+
+    }
+    public static void main(String[] args) {
+
+        Arrays.fill(nums, "");
+
+        nums[1] = "one";
+        nums[2] = "two";
+        nums[3] = "three";
+        nums[4] = "four";
+        nums[5] = "five";
+        nums[6] = "six";
+        nums[7] = "seven";
+        nums[8] = "eight";
+        nums[9] = "nine";
+        nums[10] = "ten";
+        nums[11] = "eleven";
+        nums[12] = "twelve";
+        nums[13] = "thirteen";
+        nums[14] = "fourteen";
+        nums[15] = "fifteen";
+        nums[16] = "sixteen";
+        nums[17] = "seventeen";
+        nums[18] = "eighteen";
+        nums[19] = "nineteen";
+        nums[20] = "twenty";
+        nums[30] = "thirty";
+        nums[40] = "forty";
+        nums[50] = "fifty";
+        nums[60] = "sixty";
+        nums[70] = "seventy";
+        nums[80] = "eighty";
+        nums[90] = "ninety";
+
+        solve();
+    }
+}
